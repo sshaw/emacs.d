2079632f3f37e496f46d0a9b5efd297f2fe546bb
sshaw <skye.shaw@gmail.com>
2015-03-14 13:42:45 -0400

    Use progn in eval-after-load to set grep-files-aliases

--- init/init-perl.el
+++ init/init-perl.el
@@ -1,2 +1,9 @@
+(defalias 'perl-mode 'cperl-mode)
+
+(eval-after-load 'grep
+  '(progn
+     (add-to-list 'grep-files-aliases
+		  '("pl" . "*.pm *.pl *.t"))))
+
 (add-to-list 'auto-mode-alist
-	     '("\\(?:\\.t\\|^cpanfile\\)\\'" . perl-mode))
+	     '("\\(?:\\.t\\|^cpanfile\\)\\'" . cperl-mode))
--- init/init-ruby.el
+++ init/init-ruby.el
@@ -1,14 +1,15 @@
 (eval-after-load 'rspec-mode
   '(rspec-install-snippets))
 
+(eval-after-load 'grep
+  '(progn
+     (add-to-list 'grep-files-aliases
+		  '("rb" . "*.rb *.rake *.thor"))
+     (add-to-list 'grep-files-aliases
+		  '("rhtml" . "*.erb *.haml *.slim"))))
+
 (add-to-list 'auto-mode-alist
 	     '("\\.\\(?:gemspec\\|irbrc\\|gemrc\\|pryrc\\|rake\\|rb\\|ru\\|thor\\)\\'" . ruby-mode))
 
 (add-to-list 'auto-mode-alist
 	     '("\\(Capfile\\|Gemfile\\(?:\\.[a-zA-Z0-9._-]+\\)?\\|[rR]akefile\\)\\'"   . ruby-mode))
-
-(add-to-list 'grep-files-aliases
-	     '("rb" . "*.rb *.rake *.thor"))
-
-(add-to-list 'grep-files-aliases
-	     '("rhtml" . "*.erb *.haml *.slim"))
