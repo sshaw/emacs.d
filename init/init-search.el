099ab7e96e1722879a9b3edf3aae0f3713bdc5b4
sshaw <skye.shaw@gmail.com>
2016-01-16 16:42:52 -0500

    Key binding for find-dired

--- init/init-search.el
+++ init/init-search.el
@@ -1,1 +1,6 @@
-(fullframe rgrep quit-window)
+;; fullframe has problems with interactive functions https://github.com/tomterl/fullframe/issues/14
+;;(fullframe rgrep quit-window)
+;;(global-set-key (kbd "C-x r") 'rgrep)
+
+;; Default binding calls set-fill-column
+(global-set-key (kbd "C-x f") 'find-dired)
