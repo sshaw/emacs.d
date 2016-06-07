324e4e87d420ca746b827715336630c14bf05542
sshaw <skye.shaw@gmail.com>
2016-06-07 01:23:24 -0400

    Simplify text-mode hooks

--- init/init-text-mode.el
+++ init/init-text-mode.el
@@ -1,5 +1,3 @@
 (add-hook 'text-mode-hook 'column-number-mode)
-(add-hook 'text-mode-hook
-	  (lambda () (abbrev-mode)))
-(add-hook 'markdown-mode-hook
-	  (lambda () (flyspell-mode)))
+(add-hook 'text-mode-hook 'abbrev-mode)
+(add-hook 'text-mode-hook 'flyspell-mode)
