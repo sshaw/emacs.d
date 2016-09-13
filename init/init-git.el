a2b90c7df2b497a73a19e2caaa4c10cf4ccdcfa0
sshaw <skye.shaw@gmail.com>
2016-09-12 22:34:03 -0400

    Add key bindings for git-link

--- init/init-git.el
+++ init/init-git.el
@@ -7,3 +7,7 @@
 (global-set-key (kbd "C-x g") 'magit-status)
 
 (fullframe magit-status magit-mode-quit-window)
+
+(global-set-key (kbd "C-c g l") 'git-link)
+(global-set-key (kbd "C-c g c") 'git-link-commit)
+(global-set-key (kbd "C-c g h") 'git-link-homepage)
