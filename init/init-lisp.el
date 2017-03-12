414558143f25e3605eaaac90b06641ea266f0f42
sshaw <skye.shaw@gmail.com>
2017-03-12 15:49:22 -0400

    Don't use lambda in emacs-lisp-mode-hook

--- init/init-lisp.el
+++ init/init-lisp.el
@@ -1,9 +1,8 @@
 (add-to-list 'auto-mode-alist
 	     '("\\(?:abbrev_defs\\|Cask\\(?:\\.[a-zA-Z0-9._-]+\\)?\\)\\'" . emacs-lisp-mode))
 
-(add-hook 'emacs-lisp-mode-hook
-	  (lambda ()
-	    (rainbow-delimiters-mode)
-	    (eldoc-mode)))
+(add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
+(add-hook 'emacs-lisp-mode-hook 'nameless-mode)
+(add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
 
 (add-hook 'ielm-mode-hook 'eldoc-mode)
