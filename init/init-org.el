2e9015f2fe2e1440c68f50014ff2bfe7ff778fa0
sshaw <skye.shaw@gmail.com>
2017-03-12 21:00:36 -0400

    Binding for org-agenda

--- init/init-org.el
+++ init/init-org.el
@@ -4,6 +4,7 @@
      (require 'ox-reveal)))
 
 (global-set-key (kbd "C-c c") 'org-capture)
+(global-set-key (kbd "C-c a") 'org-agenda)
 
 (setq org-confirm-babel-evaluate nil)
 (setq org-capture-templates
