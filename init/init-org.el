2083a6f9ea058726ee98fac7bd13af9ede17c024
sshaw <skye.shaw@gmail.com>
2017-08-04 11:49:53 -0400

    Better org export settings

--- init/init-org.el
+++ init/init-org.el
@@ -6,6 +6,9 @@
 (global-set-key (kbd "C-c c") 'org-capture)
 (global-set-key (kbd "C-c a") 'org-agenda)
 
+(setq org-export-with-toc nil)
+(setq org-export-with-section-numbers nil)
+
 (setq org-confirm-babel-evaluate nil)
 (setq org-capture-templates
       '(("t" "Todo" entry (file "") "* TODO %?\n  %i\n  %a")))
