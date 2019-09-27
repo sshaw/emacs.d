8ddc3766d02ddccd6bd2b90855386b2a0b1afb04
sshaw <skye.shaw@gmail.com>
2019-09-27 16:36:01 -0400

    JS + TypeScript stuff

--- init/init-javascript.el
+++ init/init-javascript.el
@@ -1,6 +1,11 @@
 (eval-after-load 'coffee-mode
   '(setq coffee-tab-width 2))
 
+(eval-after-load 'grep
+  '(progn
+     (add-to-list 'grep-files-aliases
+		  '("js" . "*.js *.jsx *.ts *.tsx"))))
+
 (add-to-list 'auto-mode-alist
 	     '("\\.coffee\\.erb\\'" . coffee-mode))
 
